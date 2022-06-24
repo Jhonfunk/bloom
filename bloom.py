5caec78586b9622a43eba0c5acaebc55017fb144
Jhonfunk <78594391+Jhonfunk@users.noreply.github.com>
2022-06-24 23:09:30 +0800

    Update bloom.py

--- bloom.py
+++ bloom.py
@@ -1,5 +1,6 @@
 #!/bin/python
-with open('bloom.dat', 'r') as f:
-  print(f.read())
-with open('bloom2.dat', 'r') as f2:
-  print(f2.read())
+list = os.listdir(rootdir)
+for name in list:
+  if os.path.isfile(name):
+    with open(name, 'r') as f:
+      print(f.read())
